4d51d23f403e357c8448803f45114a05bc2f2dc7
tobymaxwell <tmaxwell@uoregon.edu>
2018-09-23 09:09:28 -0600

    update

--- isotopes redo.R
+++ isotopes redo.R
@@ -1,7 +1,7 @@
 library(plyr)
-setwd("/Users/Maxwell/OneDrive - University Of Oregon/Oregon/Papers/Garrett JGR/")
-goeiso<-read.csv("/Users/Maxwell/OneDrive - University Of Oregon/Oregon/Papers/Garrett JGR/GOE_isotopes.csv")
-goeppt<-read.csv("/Users/Maxwell/OneDrive - University Of Oregon/Oregon/Papers/Garrett JGR/GOE_annual_PT.csv")
+setwd("/Users/tobymaxwell/OneDrive - University Of Oregon/Oregon/Papers/Garrett JGR/")
+goeiso<-read.csv("/Users/tobymaxwell/OneDrive - University Of Oregon/Oregon/Papers/Garrett JGR/GOE_isotopes.csv")
+goeppt<-read.csv("/Users/tobymaxwell/OneDrive - University Of Oregon/Oregon/Papers/Garrett JGR/GOE_annual_PT.csv")
 str(goeiso)
 str(goeppt)
 
@@ -42,13 +42,13 @@ anova(tmod)
 tuk<-HSD.test(tmod, trt="st", DFerror=164,MSerror=0.7464, group=T)
 tuk<-data.frame(tuk$groups)
 tuk<-tuk[order(rownames(tuk)),]
-tuk
+str(tuk)
 median<-ddply(goe4, .(site, treatment), summarise, 
                d18Omed<-median(d18O))
 tuk$med<-median$..1
               
 ggplot(goe4, aes(y=(d18O), x=st, shape = treatment))+geom_boxplot()+theme_classic()+
-  annotate("text", label =tuk$groups, y=tuk$med-0.1, x=c(1:12))
+  annotate("text", label =tuk$trt, y=tuk$med-0.1, x=c(1:12))
 
 tmod<-aov(d18Onorm~st, goe4[goe4$treatment!="control",])
 anova(tmod)
@@ -61,7 +61,7 @@ median<-ddply(goe4[goe4$treatment!="control",], .(site, treatment), summarise,
 tuk$med<-median$..1
 
 ggplot(goe4[goe4$treatment!="control",], aes(y=(d18Onorm),x=st))+geom_boxplot()+theme_classic()+
-  annotate("text", label =tuk$groups, y=tuk$med-0.005, x=c(1:9))
+  annotate("text", label =tuk$trt, y=tuk$med-0.005, x=c(1:9))
 
 ggplot(goe4[goe4$treatment!="control",], aes(y=(d18Onorm), x=year))+
   geom_line(data=goe4[goe4$treatment!="control",], lty="dotdash")+geom_errorbar(data=goe4[goe4$treatment!="control",], width = 0.2, aes(ymin=d18Onorm-d18Onormse, ymax=d18Onorm+d18Onormse))+
@@ -70,15 +70,32 @@ ggplot(goe4[goe4$treatment!="control",], aes(y=(d18Onorm), x=year))+
 
 ####Mixed Effects####
 library(lme4)
-goe.mem<-merge(goeiso, goeppt)
-goeall<-read.csv("GOE_tree_all_data.csv")
-str(goeall)
 
-mem1<-lmer(d18O~iWUE+avg_t+annual_mm+(1|year)+)
+#firest reorganize climate data
+climate<-read.csv("GOE_tree_all_climate.csv")
+str(climate)
+climate$water_qtr<-as.factor(climate$water_qtr)
+climate<-ddply(climate, .(year, site, water_qtr), summarise, 
+               annual_mm=mean(annual_mm),
+               avg_t=mean(avg_t),
+               WY_Q_pct=mean(WY_Q_pct),
+               rain_WY_mm = mean(rain_WY_mm),
+               max_t = mean(max_t),
+               min_t = mean(min_t))
+clim.wide<-reshape(data=na.omit(climate), 
+        idvar = c("year", "site"), timevar = c("water_qtr"),
+        direction = "wide")
+str(clim.wide)
+clim.wide
+goe.mem<-merge(goeiso, clim.wide)
+str(goe.mem)
 
+mem1<-lmer(scale(d18O)~scale(iWUE)+scale(meanBA)+scale(avg_t.2)+scale(avg_t.4)+scale(rain_WY_mm.4)+treatment+(1|year)+(rain_WY_mm.4|site)+(1|tree), goe.mem)
+summary(mem1)
+library(sjPlot)
+sjt.lmer(mem1)
 
-
-
+sjp.lmer(mem1, y.offset = .4, type="rs.ri")
 
 elkHFts<-goe4[goe4$site=="Elkhorn"&goe4$treatment=="HF",]
 elkHFts<-elkHFts[-1]
